55e5b9d2d52f73f79bd70d5463c735156bd9185d
sgaestel <sgaestel@sierrawireless.com>
2017-07-06 12:00:45 +0200

    PLTFRS-8006 - Format with prettier using 2 spaces tabs

--- techHttp.js
+++ techHttp.js
@@ -6,43 +6,43 @@ var techTime = require('node-tech-time');
 var logger = require('node-tech-logger');
 
 module.exports = function(mockRequest) {
-    var request = mockRequest || BPromise.promisifyAll(require('request'));
+  var request = mockRequest || BPromise.promisifyAll(require('request'));
 
-    var emitter = new events.EventEmitter();
+  var emitter = new events.EventEmitter();
 
-    function wrapRequest(ruuid, fnName, options, category) {
-        techRuuid.check(ruuid);
+  function wrapRequest(ruuid, fnName, options, category) {
+    techRuuid.check(ruuid);
 
-        category = category || 'core';
+    category = category || 'core';
 
-        var start = techTime.start();
+    var start = techTime.start();
 
-        logger.debug('[http] Request options', options);
-        logger.debug('[http] Request headers', options.headers);
+    logger.debug('[http] Request options', options);
+    logger.debug('[http] Request headers', options.headers);
 
-        return request[fnName](options.url, options).spread(function(response, body) {
-            emitter.emit('http', {
-                category: category,
-                ruuid: ruuid || 'unknown',
-                url: options.url,
-                duration: techTime.end(start)
-            });
+    return request[fnName](options.url, options).spread(function(response, body) {
+      emitter.emit('http', {
+        category: category,
+        ruuid: ruuid || 'unknown',
+        url: options.url,
+        duration: techTime.end(start)
+      });
 
-            if (response && _.includes([200, 201, 202, 203, 204, 205, 206], response.statusCode)) {
-                return body;
-            } else {
-                return BPromise.reject({
-                    response: response ? response.statusCode : response,
-                    body: body
-                });
-            }
+      if (response && _.includes([200, 201, 202, 203, 204, 205, 206], response.statusCode)) {
+        return body;
+      } else {
+        return BPromise.reject({
+          response: response ? response.statusCode : response,
+          body: body
         });
-    }
+      }
+    });
+  }
 
-    return {
-        on: emitter.on.bind(emitter),
+  return {
+    on: emitter.on.bind(emitter),
 
-        /**
+    /**
          * http get
          *
          * @param options.url
@@ -52,27 +52,27 @@ module.exports = function(mockRequest) {
          * @param [options.headers]
          * @param [options.json]
          */
-        get: function wrapGet(ruuid, options) {
-            if (_.isString(options)) {
-                options = {
-                    url: options
-                };
-            }
+    get: function wrapGet(ruuid, options) {
+      if (_.isString(options)) {
+        options = {
+          url: options
+        };
+      }
 
-            return wrapRequest(
-                ruuid,
-                'getAsync',
-                _.assignIn(
-                    {
-                        json: true
-                    },
-                    options
-                ),
-                options.category
-            );
-        },
+      return wrapRequest(
+        ruuid,
+        'getAsync',
+        _.assignIn(
+          {
+            json: true
+          },
+          options
+        ),
+        options.category
+      );
+    },
 
-        /**
+    /**
          * http post
          *
          * @param options.url
@@ -83,21 +83,21 @@ module.exports = function(mockRequest) {
          * @param [options.json]
          * @param [options.body]
          */
-        post: function wrapPost(ruuid, options) {
-            return wrapRequest(
-                ruuid,
-                'postAsync',
-                _.assignIn(
-                    {
-                        json: true
-                    },
-                    options
-                ),
-                options.category
-            );
-        },
+    post: function wrapPost(ruuid, options) {
+      return wrapRequest(
+        ruuid,
+        'postAsync',
+        _.assignIn(
+          {
+            json: true
+          },
+          options
+        ),
+        options.category
+      );
+    },
 
-        /**
+    /**
          * http delete
          *
          * @param options.url
@@ -108,21 +108,21 @@ module.exports = function(mockRequest) {
          * @param [options.json]
          * @param [options.body]
          */
-        delete: function wrapDelete(ruuid, options) {
-            return wrapRequest(
-                ruuid,
-                'delAsync',
-                _.assignIn(
-                    {
-                        json: true
-                    },
-                    options
-                ),
-                options.category
-            );
-        },
+    delete: function wrapDelete(ruuid, options) {
+      return wrapRequest(
+        ruuid,
+        'delAsync',
+        _.assignIn(
+          {
+            json: true
+          },
+          options
+        ),
+        options.category
+      );
+    },
 
-        /**
+    /**
          * http put
          *
          * @param options.url
@@ -133,18 +133,18 @@ module.exports = function(mockRequest) {
          * @param [options.json]
          * @param [options.body]
          */
-        put: function wrapPut(ruuid, options) {
-            return wrapRequest(
-                ruuid,
-                'putAsync',
-                _.assignIn(
-                    {
-                        json: true
-                    },
-                    options
-                ),
-                options.category
-            );
-        }
-    };
+    put: function wrapPut(ruuid, options) {
+      return wrapRequest(
+        ruuid,
+        'putAsync',
+        _.assignIn(
+          {
+            json: true
+          },
+          options
+        ),
+        options.category
+      );
+    }
+  };
 };
--- test/techHttpSpec.js
+++ test/techHttpSpec.js
@@ -4,73 +4,73 @@ var assert = require('chai').assert;
 var BPromise = require('bluebird');
 
 describe('tech-http', function() {
-    it('accepts get with a single arg', function() {
-        var mockRequest = {
-            getAsync: function(url, opts) {
-                assert.equal(url, 'http://foo:8080/api/bar/baz');
-                return BPromise.resolve([
-                    {
-                        statusCode: 200
-                    },
-                    {}
-                ]);
-            }
-        };
+  it('accepts get with a single arg', function() {
+    var mockRequest = {
+      getAsync: function(url, opts) {
+        assert.equal(url, 'http://foo:8080/api/bar/baz');
+        return BPromise.resolve([
+          {
+            statusCode: 200
+          },
+          {}
+        ]);
+      }
+    };
 
-        var http = techHttp(mockRequest);
+    var http = techHttp(mockRequest);
 
-        return http.get('avop-ruuid-45', 'http://foo:8080/api/bar/baz');
-    });
+    return http.get('avop-ruuid-45', 'http://foo:8080/api/bar/baz');
+  });
 
-    it('measures the duration of a call', function() {
-        // Ad-hoc mock for the 'request' library
-        var mockRequest = {
-            getAsync: function(url, opts) {
-                assert.equal(url, 'http://foo:8080/api/bar/baz', 'Url should be set');
-                assert.equal(opts.headers['X-foo'], 'bar');
-                assert.ok(opts.json, 'Json should be requested');
-                // Simulate a requests that takes a few milliseconds to run
-                return new BPromise(function(resolve, reject) {
-                    setTimeout(function() {
-                        resolve([
-                            {
-                                statusCode: 200
-                            },
-                            {
-                                foo: 'bar'
-                            }
-                        ]);
-                    }, 100);
-                });
-            }
-        };
-
-        var http = techHttp(mockRequest);
-
-        var duration = null;
-        http.on('http', function(what) {
-            assert.ok(what.ruuid === 'avop-ruuid-42', 'Ruuid should be set');
-            assert.equal(what.category, 'cat');
-            duration = what.duration;
+  it('measures the duration of a call', function() {
+    // Ad-hoc mock for the 'request' library
+    var mockRequest = {
+      getAsync: function(url, opts) {
+        assert.equal(url, 'http://foo:8080/api/bar/baz', 'Url should be set');
+        assert.equal(opts.headers['X-foo'], 'bar');
+        assert.ok(opts.json, 'Json should be requested');
+        // Simulate a requests that takes a few milliseconds to run
+        return new BPromise(function(resolve, reject) {
+          setTimeout(function() {
+            resolve([
+              {
+                statusCode: 200
+              },
+              {
+                foo: 'bar'
+              }
+            ]);
+          }, 100);
         });
+      }
+    };
 
-        return http
-            .get('avop-ruuid-42', {
-                url: 'http://foo:8080/api/bar/baz',
-                category: 'cat',
-                headers: {
-                    'X-foo': 'bar'
-                }
-            })
-            .then(
-                function(result) {
-                    assert.ok(result.foo === 'bar', 'request apis should have been wrapped');
-                    assert.ok(parseFloat(duration.ms) > 90, 'the request time should have been logged');
-                    assert.ok(parseFloat(duration.ms) < 250, 'the request time should not be that huge !');
-                },
-                function(error) {
-                    throw new Error('Rejected with ' + JSON.stringify(error));
-                }
-            );
+    var http = techHttp(mockRequest);
+
+    var duration = null;
+    http.on('http', function(what) {
+      assert.ok(what.ruuid === 'avop-ruuid-42', 'Ruuid should be set');
+      assert.equal(what.category, 'cat');
+      duration = what.duration;
     });
+
+    return http
+      .get('avop-ruuid-42', {
+        url: 'http://foo:8080/api/bar/baz',
+        category: 'cat',
+        headers: {
+          'X-foo': 'bar'
+        }
+      })
+      .then(
+        function(result) {
+          assert.ok(result.foo === 'bar', 'request apis should have been wrapped');
+          assert.ok(parseFloat(duration.ms) > 90, 'the request time should have been logged');
+          assert.ok(parseFloat(duration.ms) < 250, 'the request time should not be that huge !');
+        },
+        function(error) {
+          throw new Error('Rejected with ' + JSON.stringify(error));
+        }
+      );
+  });
 });
