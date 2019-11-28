68740f3e3dadb3c79056b980adaae4265fd7c41e
LESCOT Jacques <jlescot@sierrawireless.com>
2019-11-28 17:40:04 +0100

    Code review - Use arrow function + indentation is using 2 spaces again

--- techHttp.js
+++ techHttp.js
@@ -2,27 +2,27 @@ const _ = require('lodash');
 const BPromise = require('bluebird');
 
 module.exports = mockRequest => {
-    const request = mockRequest || BPromise.promisifyAll(require('request'));
+  const request = mockRequest || BPromise.promisifyAll(require('request'));
 
-    const wrapRequest = (fnName, options) => {
-        return request[fnName](options.url, options).spread(function(response, body) {
-            if (response && _.includes([200, 201, 202, 203, 204, 205, 206], response.statusCode)) {
-                return body;
-            }
-            return BPromise.reject({ body, response: response ? response.statusCode : response });
-        });
-    };
+  const wrapRequest = (fnName, options) => {
+    return request[fnName](options.url, options).spread((response, body) => {
+      if (response && _.includes([200, 201, 202, 203, 204, 205, 206], response.statusCode)) {
+        return body;
+      }
+      return BPromise.reject({ body, response: response ? response.statusCode : response });
+    });
+  };
 
-    return {
-        get: (ruuid, options) =>
-            _.isString(options)
-                ? wrapRequest('getAsync', { json: true, url: options })
-                : wrapRequest('getAsync', _.assignIn({ json: true }, options)),
+  return {
+    get: (ruuid, options) =>
+      _.isString(options)
+        ? wrapRequest('getAsync', { json: true, url: options })
+        : wrapRequest('getAsync', _.assignIn({ json: true }, options)),
 
-        post: (ruuid, options) => wrapRequest('postAsync', _.assignIn({ json: true }, options)),
+    post: (ruuid, options) => wrapRequest('postAsync', _.assignIn({ json: true }, options)),
 
-        delete: (ruuid, options) => wrapRequest('delAsync', _.assignIn({ json: true }, options)),
+    delete: (ruuid, options) => wrapRequest('delAsync', _.assignIn({ json: true }, options)),
 
-        put: (ruuid, options) => wrapRequest('putAsync', _.assignIn({ json: true }, options))
-    };
+    put: (ruuid, options) => wrapRequest('putAsync', _.assignIn({ json: true }, options))
+  };
 };
